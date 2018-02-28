e5027207a6d6d5187883fc3561326ce7f78b2083
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-02-27 16:26:21 -0800

    implemented createMap function

---
+++ map.js
@@ -0,0 +1,11 @@
+function createMap(width, height) {
+  d3.select("#map")
+      .attr("width", width)
+      .attr("height", height)
+    .append("text")
+      .attr("x", width / 2)
+      .attr("y", "1em")
+      .attr("font-size", "1.5em")
+      .style("text-anchor", "middle")
+      .classed("map-title", true);
+}
